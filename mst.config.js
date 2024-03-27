dec9c83402b4e1ec76640bdc775f3271acf2abf6
JingHai-Lingyun <m56042764@163.com>
2024-03-27 20:21:42 +0800

    Release v1.0.1

--- mst.config.js
+++ mst.config.js
@@ -1,2 +1,3 @@
-const server_host = "" // 将这里设置为你所使用的 MST-Server 服务的 IP 或域名
-const server_port = "" // 将这里设置为你所使用的 MST-Server 服务的端口
+const server_https = false // 如果你的服务器域名支持 https，填写 true，否则填写 false
+const server_host = "localhost"    // 将这里设置为你所使用的 MST-Server 服务的 IP 或域名
+const server_port = "6001"    // 将这里设置为你所使用的 MST-Server 服务的端口
